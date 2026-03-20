959eb765030865798c477186016d5a2a558c6d3f
gayatri-kulkarni24 <gaytrikulkarni616@gmail.com>
2026-03-20 21:16:06 +0530

    Added the backend integration

--- todo-list-server/index.js
+++ todo-list-server/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 app.use(express.json());
-app.use(cors("*"));
+
 const connectDB = require('./config/db');
 const todoRoutes = require('./routes/todoRoutes');
 
